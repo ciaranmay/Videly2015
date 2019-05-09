bf9b774ff6617381a535032bdbf2c690c4ed8161
ciaranmay <cpmcoder@gmail.com>
2019-05-09 09:55:13 +0100

    Add project files.

---
+++ Vidley2015/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Vidley2015.Startup))]
+namespace Vidley2015
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
